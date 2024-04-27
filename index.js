90bd6c37d4333a320e0f67328a3129b97b6bdd03
Ruturaj <vruturaj633@gmail.com>
2024-04-27 10:52:01 +0530

    Adding index.html and index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+// Write your code below:
+let mainHeading = document.getElementById('main-heading');
+mainHeading.textContent ="Fruit World";
+mainHeading.style.color="orange";
+
+let div = document.getElementById('header');
+div.style.backgroundColor ="green";
+div.style.borderBottom="1px solid orange";
+
+let heading = document.getElementById('basket-heading');
+ heading.style.color ="green";
+
+ let thnx = document.getElementById('thanks');
+ thnx.innerHTML="<p>Please visit us again</p>";
