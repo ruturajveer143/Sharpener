6b74b5f322109e423a18c63fd2559f9ffaf2c45d
Ruturaj <vruturaj633@gmail.com>
2024-04-29 12:22:19 +0530

    Task 7 in this new branch

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-const mainHeading = document.querySelector('#main-heading');
-mainHeading.style.textAlign='center';
+// Task 1:
 
- 
+const h3 = document.createElement('h3');
+h3.textContent = 'Buy high quality organic fruits online';
 
-const fruits = document.querySelector('.fruits');
-fruits.style.backgroundColor='grey';
-fruits.style.padding='30px';
-fruits.style.margin='30px';
-fruits.style.width='50%';
-fruits.style.borderRadius = '5px';
-fruits.style.listStyleType='none';
+const mainHeading = document.getElementById('main-heading');
+mainHeading.parentNode.insertBefore(h3, mainHeading.nextSibling);
 
-const basketHeading = document.querySelector('h2');
-basketHeading.style.marginLeft = '30px';
-basketHeading.style.color='brown';
+// Task 2:
+h3.style.fontStyle = 'italic';
 
-const fruitItems =document.querySelectorAll('.fruit');
-for(let i = 0; i<fruitItems.length; i++){
-  fruitItems[i].style.backgroundColor = 'brown';
-  fruitItems[i].style.color = 'white';
-  fruitItems[i].style.padding='10px';
-  fruitItems[i].style.margin='10px';
-  fruitItems[i].borderRadius = '5px';
-}
+// Task 3:
+const para = document.createElement('p');
+para.textContent = 'Total fruits: 4';
 
-const OddFruitItems = document.querySelectorAll('.fruit:nth-child(odd)');
-for(let i =0;i<OddFruitItems.length;i++){
-  OddFruitItems[i].style.backgroundColor = 'lightgray';
-}
+// Task 4: On this paragraph tag, set an id of "fruits-total".
+para.id = 'fruits-total';
+const secondDiv = document.querySelector('div:nth-child(2)');
 
+const urlist = secondDiv.querySelector('ul');
+secondDiv.insertBefore(para, urlist);
