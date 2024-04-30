136d208cfa8393d1309ecf59da6bbc9fb3c41ba6
Ruturaj <vruturaj633@gmail.com>
2024-04-30 07:58:40 +0530

    Task 11 in this new branch

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
-const form = document.querySelector('form');
-
-const fruits = document.querySelector('.fruits');
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-
-    const inputValue = document.getElementById('fruit-to-add');
-
-    //Li-adding
-    const newLi = document.createElement('li');
-
-   // newLi.innerHTML = inputValue.value + '<button class="delete-btn">x</button>' + '<button class="edit-btn">Edit</button>';
-
-
-     const newLitext = document.createTextNode(inputValue.value);
- newLi.appendChild(newLitext);
- newLi.className = 'fruit';
-
-//deltetBTN
- const deleteBtn = document.createElement('button');
- const deleteBtnText = document.createTextNode('x');
- deleteBtn.appendChild(deleteBtnText);
- deleteBtn.className = 'delete-btn';
- newLi.appendChild(deleteBtn);
-
-
-//editBTN
-
-const editBtn = document.createElement('button');
-const editBtnText = document.createTextNode('Edit');
- editBtn.appendChild(editBtnText);
- editBtn.className = 'edit-btn';
- newLi.appendChild(editBtn);
-
-
-fruits.appendChild(newLi);
-
-
+// Add input element for fruit description inside form
+const filter = document.getElementById("filter");
+filter.addEventListener("keyup", function (event) 
+                  {
+    const textEntered = event.target.value.toLowerCase();
+    const fruitItems = document.getElementsByClassName("fruit");
+    for (let i = 0; i < fruitItems.length; i++) {
+        const currentFruitText = fruitItems[i].firstChild.textContent.toLowerCase();
+        if (currentFruitText.indexOf(textEntered) === -1) 
+        {
+            fruitItems[i].style.display = "none";
+        } else 
+        {
+            fruitItems[i].style.display = "flex";
+        }
+    }
 });
 
-
-fruits.addEventListener('click', function(event){
-    if(event.target.classList.contains('delete-btn')){
-        const fruitToDelete = event.target.parentElement;
-        fruits.removeChild(fruitToDelete);
-    };
-    
-});
