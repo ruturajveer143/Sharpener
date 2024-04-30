c50052caf044622a7310f90d47a47c9ebe719d96
Ruturaj <vruturaj633@gmail.com>
2024-04-30 08:01:35 +0530

    Task 12 in this new branch

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// Add input element for fruit description inside form
-const filter = document.getElementById("filter");
-filter.addEventListener("keyup", function (event) 
-                  {
-    const textEntered = event.target.value.toLowerCase();
-    const fruitItems = document.getElementsByClassName("fruit");
-    for (let i = 0; i < fruitItems.length; i++) {
-        const currentFruitText = fruitItems[i].firstChild.textContent.toLowerCase();
-        if (currentFruitText.indexOf(textEntered) === -1) 
-        {
-            fruitItems[i].style.display = "none";
-        } else 
-        {
-            fruitItems[i].style.display = "flex";
-        }
-    }
-});
+// Write your code below:
 
+const myForm = document.querySelector('form');
+
+    myForm.addEventListener('submit', handleFormSubmit( 
+        event.preventDefault();
+
+        const username = myForm.elements['username'].value;
+        const email = myForm.elements['email'].value;
+        const phone = myForm.elements['phone'].value;
+
+        // Storing data in local storage
+        localStorage.setItem('Username', username);
+        localStorage.setItem('Email', email);
+        localStorage.setItem('Phone', phone);
+
+    );
